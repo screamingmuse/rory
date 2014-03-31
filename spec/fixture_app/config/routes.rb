5e3263b71d0aa4d9c3d8b06d344efe76e7f0a44c
Ravi Gadad <ravi@renewfund.com>
2014-03-31 02:19:03 -0700

    Fixture routes mod to better demonstrate scoping

--- spec/fixture_app/config/routes.rb
+++ spec/fixture_app/config/routes.rb
@@ -1,11 +1,13 @@
 Fixture::Application.set_routes do
   match 'foo/:id/bar', :to => 'foo#bar', :methods => [:get, :post]
   match 'this/:path/is/:very_awesome', :to => 'awesome#rad'
-  scope :module => 'goose' do
-    match 'lumpies/:lump', :to => 'lumpies#show', :methods => [:get]
-  end
-  scope :module => 'goose/wombat' do
-    match 'rabbits/:chew', :to => 'rabbits#chew', :methods => [:get]
+  scope :method => [:get] do
+    scope :module => 'goose' do
+      match 'lumpies/:lump', :to => 'lumpies#show'
+    end
+    scope :module => 'goose/wombat' do
+      match 'rabbits/:chew', :to => 'rabbits#chew'
+    end
   end
   match '/', :to => 'root#vegetable', :methods => [:get]
   match '/', :to => 'root#no_vegetable', :methods => [:delete]
