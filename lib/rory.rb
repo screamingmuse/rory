e5fb79256bbf6fa49030b12500c09d7c371bfaa4
David Begin <davidmichaelbe@gmail.com>
2014-07-17 21:56:53 -0700

    updated to remove ENV monkey patching

--- lib/rory.rb
+++ lib/rory.rb
@@ -1,4 +1,10 @@
-ENV['RORY_STAGE'] ||= ENV['RACK_ENV'] || 'development'
+if ENV['RORY_STAGE']
+  puts "can you please not use this"
+  ENV['RORY_ENV'] = ENV['RORY_STAGE']
+end
+
+# ENV['RORY_STAGE'] ||= ENV['RACK_ENV'] || 'development'
+ENV['RACK_ENV'] || 'development'
 
 require 'yaml'
 require 'sequel'
@@ -7,7 +13,7 @@ require 'rory/dispatcher'
 require 'rory/route'
 require 'rory/support'
 require 'rory/controller'
-require 'support/env_deprecation'
+# require 'support/env_deprecation'
 
 module Rory
   class << self
