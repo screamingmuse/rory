50f501a62efeb200d47a6670df80019a0618fd19
Ravi Gadad <ravi@renewfund.com>
2015-03-12 03:21:51 -0700

    Add a missing Route spec

--- spec/lib/rory/route_spec.rb
+++ spec/lib/rory/route_spec.rb
@@ -5,4 +5,11 @@ describe Rory::Route do
       expect(route.name).to eq 'pigeons_index'
     end
   end
+
+  describe "#path_params" do
+    it "extracts params from path into hash" do
+      route = described_class.new('/spoons/:spoon_id/forks/:fork_id', :to => 'cutlery#index')
+      expect(route.path_params('spoons/4/forks/yay')).to eq({ :spoon_id => "4", :fork_id => "yay" })
+    end
+  end
 end
